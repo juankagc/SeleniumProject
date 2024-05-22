43e15d6c223c8c544c5034362373110f7daa3779
Juan Gimenez <juan.gimenez@oktana.com>
2024-05-22 10:54:05 -0400

    First commit

---
+++ src/main/java/org/javacourse/App.java
@@ -0,0 +1,13 @@
+package org.javacourse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
